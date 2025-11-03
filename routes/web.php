8a46c2737fd8dbd7b7547b26dc0c1336c0565b67
Joro <rabearisoabryan6827@gmail.com>
2025-11-03 23:29:05 +0300

    Subscriber importation job crated

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Subscriber\ImportSubscriberController;
 use App\Http\Controllers\SubscriberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class, 'index']);
 
-       Route::resource('subscriber', SubscriberController::class);
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('subscriber', SubscriberController::class);
+    Route::post('subscriber/import', ImportSubscriberController::class)->name('subscriber.import');
+    
+});
