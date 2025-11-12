0988d2fe49147e2c5f3b75a1d59f215bed49a7d8
Joro <rabearisoabryan6827@gmail.com>
2025-11-12 10:17:28 +0300

    PerfomanceData, action added

---
+++ src/Domain/Mail/Actions/GetPerformanceAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Domain\Mail\Actions;
+
+use Domain\Mail\Contracts\Sendable;
+use Domain\Mail\DataTransferObjects\PerformanceData;
+use Domain\Mail\Models\SentMail;
+
+class GetPerformanceAction
+{
+    public static function execute(Sendable $sendable)
+    {
+
+     $total =  SentMail::countOf($sendable);
+     
+        return new PerformanceData(
+            total: $total,
+            open_rate: SentMail::openRate($sendable, $total),
+            click_rate: SentMail::clickRate($sendable, $total)
+        );
+    }
+}
