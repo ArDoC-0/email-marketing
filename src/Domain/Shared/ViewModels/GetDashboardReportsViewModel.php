51545e3c199889afd2c1d2d017bb011c85d449fe
Joro <rabearisoabryan6827@gmail.com>
2025-11-18 10:40:32 +0300

    DashboardReport ViewModel with all-time performance added: average clickRate, average openRate

--- src/Domain/Shared/ViewModels/GetDashboardReportsViewModel.php
+++ src/Domain/Shared/ViewModels/GetDashboardReportsViewModel.php
@@ -2,7 +2,10 @@
 
 namespace Domain\Shared\ViewModels;
 
+use Domain\Mail\DataTransferObjects\PerformanceData;
+use Domain\Mail\Models\SentMail;
 use Domain\Shared\Filters\DateFilters;
+use Domain\Shared\ValueObject\Percent;
 use Domain\Subscriber\DataTransferObjects\NewSubscriberCountData;
 use Domain\Subscriber\Models\Subscriber;
 
@@ -32,5 +35,35 @@ class GetDashboardReportsViewModel extends ViewModel
         );
     }
 
+    public function performance()
+    {
 
+        $total = SentMail::count();
+
+        return new PerformanceData(
+            total : $total,
+            click_rate: self::averageClickRate($total),
+            open_rate: self::averageOpenRate($total)
+        );
+    }
+
+    public static function averageOpenRate(int $total)
+    {
+        $opened = SentMail::whereOpened()->count();
+
+        return Percent::from(
+            $opened, 
+            $total
+        );
+    }
+
+    public static function averageClickRate(int $total)
+    {
+        $clicked = SentMail::whereClicked()->count();
+
+        return Percent::from(
+            $clicked, 
+            $total
+        );
+    }
 }
